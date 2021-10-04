390b923b4eb30831db2fe972ebe56c911f2e0645
iam-that-one <iam-that-one@users.noreply.github.com>
2021-10-04 11:37:22 +0300

    buttonsInitilize

--- app/src/main/java/com/example/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/example/kotlincalculator/MainActivity.kt
@@ -2,8 +2,29 @@ package com.example.kotlincalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btn7 : Button
+    lateinit var btn8 : Button
+    lateinit var btn9 : Button
+    lateinit var btnDiv : Button
+    lateinit var btn4 : Button
+    lateinit var btn5 : Button
+    lateinit var btn6 : Button
+    lateinit var btnMult : Button
+    lateinit var btn1 : Button
+    lateinit var btn2 : Button
+    lateinit var btn3 : Button
+    lateinit var btnMinus : Button
+    lateinit var btnPM : Button
+    lateinit var btn0 : Button
+    lateinit var btnDote : Button
+    lateinit var btnPlus : Button
+    lateinit var btnDel : Button
+    lateinit var btnC : Button
+    lateinit var btnEqual : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
